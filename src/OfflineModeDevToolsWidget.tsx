92f072bc9f6bd5875c6d7690d6c315fa247e0a95
Akos Sereg <akos.sereg@gmail.com>
2023-08-29 00:24:52 +0200

    mode selection

--- src/OfflineModeDevToolsWidget.tsx
+++ src/OfflineModeDevToolsWidget.tsx
@@ -1,7 +1,18 @@
 import * as React from "react";
 import { getConsumedLocalStorageSize, getPersistedResponsesCount } from './index';
+import { useState } from "react";
 
 const OfflineModeDevToolsWidget = () => {
+
+  const [mode, setMode] = useState(0);
+
+  // @ts-ignore
+  const handleModeSelection = (event, mode) => {
+      event.preventDefault();
+      setMode(mode);
+      return true;
+  }
+
   return (
     <div style={{
       position: 'fixed',
@@ -15,13 +26,14 @@ const OfflineModeDevToolsWidget = () => {
       width: '240px',
       fontSize: '9pt'
     }}>
-      <p>Cached responses: { (getConsumedLocalStorageSize() / 1024 / 1024).toFixed(2) } MB</p>
-      <p>
-        Count: { getPersistedResponsesCount() }
-      </p>
-      <p>
-        Mode: Capturing
-      </p>
+      <span>{ getPersistedResponsesCount() } api calls captured ({ (getConsumedLocalStorageSize() / 1024 / 1024).toFixed(2) } MB)</span>
+      <div>
+        <br/>
+        mode:&nbsp;
+        <a href="#" onClick={(event) => handleModeSelection(event,0)} style={{ fontSize: '9pt', color: '#cccccc', textDecoration: mode === 0 ? 'none' : 'underline' }}>off</a>&nbsp;|&nbsp;
+        <a href="#" onClick={(event) => handleModeSelection(event,1)} style={{ fontSize: '9pt', color: '#cccccc', textDecoration: mode === 1 ? 'none' : 'underline' }}>capturing</a>&nbsp;|&nbsp;
+        <a href="#" onClick={(event) => handleModeSelection(event,2)} style={{ fontSize: '9pt', color: '#cccccc', textDecoration: mode === 2 ? 'none' : 'underline' }}>serving</a>
+      </div>
     </div>
   );
 }
